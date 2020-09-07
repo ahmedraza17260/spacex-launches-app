e8ea565c1205791295bbd684a5d83c6589efadf4
ahmedraza17260 <ahmedraza17260gmail.com>
2020-09-07 14:12:37 +0500

    update limit b/c of loading

--- src/components/Launches/index.tsx
+++ src/components/Launches/index.tsx
@@ -1,53 +1,54 @@
-import React from 'react'
-import { gql } from 'apollo-boost';
-import { useQuery } from '@apollo/react-hooks';
-import LaunchItem from '../LaunchItem';
-import { Grid, Container, CircularProgress } from '@material-ui/core';
-
+import React from "react";
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
+import LaunchItem from "../LaunchItem";
+import { Grid, Container, CircularProgress } from "@material-ui/core";
 
 const LAUNCHES_QUERY = gql`
-    query LaunchList {
-        launchesPast(limit: 72) {
-        id
-        launch_year
-        mission_name
-        rocket {
-            rocket_name
-        }
-        launch_success
-        links {
-            mission_patch_small
-            flickr_images
-        }
-        }
-    } 
-`
+  query LaunchList {
+    launchesPast(limit: 41) {
+      id
+      launch_year
+      mission_name
+      rocket {
+        rocket_name
+      }
+      launch_success
+      links {
+        mission_patch_small
+        flickr_images
+      }
+    }
+  }
+`;
 
 const Launches = () => {
+  const { loading, error, data } = useQuery(LAUNCHES_QUERY);
 
-    const { loading, error, data } = useQuery(LAUNCHES_QUERY);
-
-    if (loading) return <div className="launches" style={{ marginTop: "40vh" }}><CircularProgress /></div>;
-    if (error) {
-        console.log(error);
-        return <p>Error happen</p>;
-    }
-    console.log(data)
+  if (loading)
     return (
-        <div className="launches">
-            <Container>
-                <Grid container spacing={3}>
-                    {
-                        data.launchesPast.map((launch: any) => (
-                            <Grid item xs={12} sm={6} key={launch.id} >
-                                <LaunchItem launch={launch} />
-                            </Grid>
-                        ))
-                    }
-                </Grid>
-            </Container>
-        </div>
-    )
-}
+      <div className="launches" style={{ marginTop: "40vh" }}>
+        <CircularProgress />
+      </div>
+    );
+  if (error) {
+    console.log(error);
+    return <p>Error happen</p>;
+  }
+  console.log(data);
+  return (
+    <div className="launches">
+      <Container>
+        <Grid container spacing={3}>
+          {data.launchesPast.map((launch: any) => (
+            <Grid item xs={12} sm={6} key={launch.id}>
+              <LaunchItem launch={launch} />
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+    </div>
+  );
+};
 
-export default Launches
+export default Launches;
